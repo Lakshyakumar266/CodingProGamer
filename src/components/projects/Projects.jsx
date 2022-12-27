1305bc482980d6d47e3045b4c1c041f895148ce2
LakshyaKumar266 <lakshyakumar0098@gmail.com>
2022-12-27 23:00:54 +0530

    projects update

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,13 +5,13 @@ function Projects() {
     <>
       <div className="p-2">
         <h3 className='text-2xl font-semibold text-zinc-700 dark:text-zinc-300 p-2 text-center'> Projects </h3>
-        <div className="projects shadow-outer space-y-2 lg:space-y-0 lg:gap-2 lg:grid lg:grid-cols-3 p-4 m-4">
+        <div className="projects shadow-outer space-y-2 lg:space-y-0 lg:gap-2 lg:grid lg:grid-cols-3 md:space-y-0 md:gap-1 md:grid md:grid-cols-2 p-4 m-4">
 
           {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((elem, index) => (
-            <a href='/' className='item-project overflow-hidden w-[400px] h-[400px] rounded'>
-              <img className='hover:brightness-50 hover:scale-125 ease-in-out duration-200 w-[400px] h-[400px]' src={`https://picsum.photos/${elem}00/${elem}00`}
-                alt="img" />
-            </a>
+              <a href='/' className='lg:p-0 p-1 item-project overflow-hidden w-[400px] h-[400px]'>
+                <img className='hover:brightness-50 lg:hover:scale-125 ease-in-out duration-200 w-[400px] h-[400px]' src={`https://picsum.photos/${elem}00/${elem}00`}
+                  alt="img" />
+              </a>
           ))}
 
         </div>
